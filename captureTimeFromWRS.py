f3109efbee3447803608cf88d1f3f9fb59d44312
liuweiseu <liuwei_seu@seu.edu.cn>
2021-10-25 14:18:47 -0700

    update captureTimeFromWRS.py.

--- captureTimeFromWRS.py
+++ captureTimeFromWRS.py
@@ -1,16 +1,32 @@
 import paramiko
+from datetime import datetime
+from datetime import timezone
+cmd0 = "/wr/bin/wr_date get"
+cmd1 = "date +'%T.%9N'"
 
-cmd = "/wr/bin/wr_date get"
 ssh=paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect('10.0.1.36',username='root',password='')
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
-result=ssh_stdout.read()
-result_str=str(result, encoding = "utf-8")
-s=result_str.split(' ')
+
+t_current = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd0)
+result0=ssh_stdout.read()
+t_host0 = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+
+ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd1)
+result1=ssh_stdout.read()
+t_host1 = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    
+result0_str=str(result0, encoding = "utf-8")
+s=result0_str.split(' ')
 d = s[3].split('\n')
-time = d[1] + ' ' +s[4]
-print(time)
-ssh.close()
+wrs_time = d[1] + ' ' +s[4]
+sys_time = str(result1, encoding = "utf-8")
+sys_time = sys_time.rstrip('\n')
+sys_time = d[1] + ' ' + sys_time
+print(wrs_time)
+print(t_host0,'\n')
+print(sys_time)
+print(t_host1,'\n')
 del(ssh,ssh_stdin, ssh_stdout, ssh_stderr)
 
